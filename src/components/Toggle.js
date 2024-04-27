feb3740d333e0e92b6b77fc3b4c28885810a9272
+ <+>
2024-04-27 21:55:15 +0300

    DOne

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn, setIsOn ]= useState(false)
+
+  function handlebutton(){
+    setIsOn((isOn=>!isOn))
+  }
+const color= isOn? "firebrick" : "white"
+
+  return <button style={{background:color} } onClick={handlebutton}>{isOn?"ON":"OFF"}</button>;
 }
 
 export default Toggle;
